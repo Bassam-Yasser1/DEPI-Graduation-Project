0f6927a65207d95171c6b8cd951b74d63a8985d7
moxhossny <mohamed.tryndmare@gmail.com>
2025-11-28 14:49:18 +0200

    making database

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,65 @@
-import 'package:depi_graduation_project/core/services/api_services/api_services.dart';
+import 'dart:io';
+
+import 'package:depi_graduation_project/core/database/tourApp_database.dart';
+import 'package:depi_graduation_project/core/utilities/assets.dart';
 import 'package:depi_graduation_project/features/auth/controllers/login_controller.dart';
 import 'package:depi_graduation_project/features/auth/presentation/views/login_view.dart';
 import 'package:depi_graduation_project/features/auth/controllers/register_controller.dart';
 import 'package:depi_graduation_project/features/auth/presentation/views/register_view.dart';
 import 'package:depi_graduation_project/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+import 'core/services/api_services/api_services.dart';
 import 'features/home/controllers/home_controller.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 1) انسخ قاعدة البيانات لأول مرة
+  await copyDatabase();
+
+  // 2) افتح قاعدة البيانات بعد النسخ
+  database = await $FloortourDatabase
+      .databaseBuilder('tourAppDB.db')
+      .build();
   runApp(const MyApp());
 }
+late tourDatabase database;
+
+Future<void> copyDatabase() async {
+  // 1) المسار اللي هتتخزن فيه القاعدة
+  final dbDir = await getDatabasesPath();
+  final dbPath = join(dbDir, 'tourAppDB.db');
+
+  // 2) لو الملف موجود → خلاص
+  if (File(dbPath).existsSync()) {
+    print("Database already exists");
+    return;
+  }
+
+  print("Copying database...");
+
+  // 3) اقرأ ملف db من الـassets
+  ByteData data = await rootBundle.load(Assets.databaseAsset);
+
+  // 4) حوله لـ List<int>
+  List<int> bytes =
+  data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+  // 5) اكتب ملف جديد في المسار
+  await File(dbPath).create(recursive: true);
+  await File(dbPath).writeAsBytes(bytes, flush: true);
+
+  print("Database copied!");
+}
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
